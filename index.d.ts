b2686eba4e72babd771e1ab46a0238cd7fdca3d5
Marshalsoft <admin@marshalsoft.net>
2022-08-17 03:15:38 +0100

    bugs fixed

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,42 @@
-declare const OnlyPass: (apiKey: string, merchantId: string) => {
-    PayNow: (amount?: number, memo?: string, isDemo?: boolean, gatewayId?: string, email?: string, phone_number?: number, gatewayName?: string, currency?: string, publicKey?: string, callback?: () => void) => Promise<void>;
-    Channels: () => Promise<any>;
-};
-export default OnlyPass;
+declare module 'onlypass-sdk' {
+    interface Gateway {
+         merchantPaymentGatewayId: number;
+         merchantId: number;
+         gatewayId: number;
+         publicKey: string;
+         status: string;
+         createdAt: string;
+         updatedAt: string;
+         gateway: {
+             gatewayId: number;
+             name: string;
+             className: string;
+             logoUrl: string;
+             status: string;
+             createdAt: string;
+             updatedAt: string;
+         };
+     }
+     
+     interface GateWayList extends Array<Gateway>{}
+     interface APIResponse {
+         data:GateWayList,
+         status?:boolean,
+         message?:string
+     }
+ const OnlyPass:(apiKey: string, merchantId: string) => {
+     PayNow:(
+         amount?: number, 
+         memo?: string, 
+         isDemo?: boolean, 
+         gatewayId?: number, 
+         email?: string, 
+         phone_number?: string, 
+         gatewayName?: string, 
+         currency?: string, 
+         publicKey?: string, 
+         callback?: () => void) => Promise<any>;
+         Channels: () => Promise<APIResponse>;
+ }
+ export default OnlyPass;
+ }
